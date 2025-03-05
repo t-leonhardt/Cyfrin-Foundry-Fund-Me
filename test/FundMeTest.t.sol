a88f00f86443a15785a4c809390208769cd0ba00
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-05 17:22:38 -0600

    Exploring tests

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTets is Test{
+    FundMe fundMe; 
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public{
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
