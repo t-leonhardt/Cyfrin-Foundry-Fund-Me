18816835a0ba5d87a25a205494109cabc8fc5d9c
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-06 01:59:15 -0600

    Corrected minor mistake in Test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTets is Test{
     FundMe fundMe; 
     function setUp() external {
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public{
@@ -19,10 +22,13 @@ contract FundMeTets is Test{
 
         console.log(fundMe.i_owner()); // FundMeTest
         console.log(msg.sender); // user address 
+        //assertEq(fundMe.i_owner(), msg.sender);
+
+
+        // assertEq(fundMe.i_owner(), address(this));
+
         assertEq(fundMe.i_owner(), msg.sender);
-
-
-        assertEq(fundMe.i_owner(), address(this));
+        // due to factorizing, user us caller again 
         
     }
 
