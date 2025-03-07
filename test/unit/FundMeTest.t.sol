fabcf0210e77fb49dc641de485b6871e0395c2af
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-07 14:13:41 -0600

    Created integration tests to fund and withdraw

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTets is Test{
     FundMe fundMe; 
